fa456de369547b8d1cdfe046b5fceff2fdcb4722
jiangxingxing <18270818853@163.com>
2017-09-06 22:44:26 +0800

    第一次提交

---
+++ solr/src/com/itheima/solr/domain/Product.java
@@ -0,0 +1,12 @@
+package com.itheima.solr.domain;
+
+/** 
+* @ClassName: Product 
+* @Description: 这是搜索产品实体类 
+* @author jiangxingxing 
+* @date 2017年9月1日 下午4:23:41 
+*  
+*/
+public class Product {
+
+}
